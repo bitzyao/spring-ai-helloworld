209c3871342603d3aa6718bd97a80b2a572da693
zyao <1215668319@qq.com>
2024-04-12 02:31:33 +0800

    项目初始化

---
+++ src/test/java/com/example/springaihelloworld/SpringAiHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaihelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
